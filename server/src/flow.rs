e3a5ceddfbe0e3610c63c5d2a1a5eaf056525950
Tymek <vonautymek@gmail.com>
2026-04-05 10:29:50 +0200

    feat: started working on policy engine

--- server/src/flow.rs
+++ server/src/flow.rs
@@ -1,2 +1,3 @@
 pub mod context;
 pub mod issuer;
+pub mod policy;
---
+++ server/src/flow/policy.rs
@@ -0,0 +1,22 @@
+use auth_core::{Factor, FactorMetadataDynamic, FactorRole};
+
+use crate::{factors::FactorName, flow::context::FlowContext};
+
+/// A policy engine that is instantiated per step in order to perform a decision.
+pub struct PolicyEngine {
+    context: FlowContext,
+}
+
+pub enum PolicyError {}
+
+impl PolicyEngine {
+    pub fn from_context(context: FlowContext) -> Self {
+        Self { context }
+    }
+
+    /// Determines if the user can use a specified factor.
+    pub fn can_use<F: FactorMetadataDynamic>(&self, factor: &F) -> Result<(), PolicyError> {
+        if factor.role() == FactorRole::MultiFactorOnly {}
+        Ok(())
+    }
+}
